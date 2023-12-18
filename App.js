5594a731b762557e1376c9205fdfdf456f4a025c
nolizwe <nolizwemasondo@gmail.com>
2023-12-18 09:31:06 +0200

    created search engine

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import WeatherSearch from "./WeatherSearch"; 
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather</h1>
+      <WeatherSearch />
+    </div>
+  );
+}
