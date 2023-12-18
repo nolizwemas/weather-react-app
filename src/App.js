9b49350af9579f49a58cb2509c0ee041bf0f5f9f
nolizwe <nolizwemasondo@gmail.com>
2023-12-18 16:01:50 +0200

    Added bootstarp

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import WeatherSearch from "./WeatherSearch";
 
 export default function App() {
   return (
+
     <div className="App">
+      <div className="container">
       <h1>Weather</h1>
       <WeatherSearch />
+    </div>
     </div>
   );
 }
