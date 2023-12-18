d0ae089b097d558d431993652bed3011f9b449ac
nolizwe <nolizwemasondo@gmail.com>
2023-12-18 16:32:30 +0200

    Added footer and api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React from "react";
-import './App.css';
-import WeatherSearch from "./WeatherSearch"; 
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
-
     <div className="App">
       <div className="container">
-      <h1>Weather</h1>
-      <WeatherSearch />
-    </div>
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="www.linkedin.com/in/nolizwe-masondo-736b38248"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nolizwe Masondo
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/nolizwemas/weather-react-app.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://rad-sprinkles-aa26b4.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
